c2de50ceee63ad776f72278b63f061d9a71cdc4d
Rodrigo Harder <rodrigo.harder@alura.com.br>
2024-12-27 13:59:31 -0300

    refactor: adição dos componentes Header, Main e Footer no página inicial

--- projeto-sala2/src/app/page.tsx
+++ projeto-sala2/src/app/page.tsx
@@ -1,7 +1,16 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+import { ModalProvider } from "@/context/ModalContext";
+
 export default function Home() {
   return (
+    <ModalProvider>
     <div>
-
+      <Header />
+      <Main />
+      <Footer />
     </div>
+    </ModalProvider>
   );
 }
