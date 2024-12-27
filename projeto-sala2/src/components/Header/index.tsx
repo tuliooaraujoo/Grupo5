19a8c2e3ec997dbf903c62389e05810f12737a21
Rodrigo Harder <rodrigo.harder@alura.com.br>
2024-12-27 13:19:55 -0300

    feat: adição do componente Header

---
+++ projeto-sala2/src/components/Header/index.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import styles from "./Header.module.css";
+import { FiMenu } from "react-icons/fi";
+import ModalButtons from "../Modal/ModalButtons";
+import ModalApplication from "../Modal/ModalApplication";
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.menu}>
+                <div className={styles.logo}>
+                    <Image
+                        src="/images/logotipo/logotipo.png"
+                        alt="Logotipo do Bytebank"
+                        width={145}
+                        height={32}
+                        className={styles.logo_Completo}
+                    />
+                    <Image
+                        src="/images/logotipo/icone_logotipo.png"
+                        alt="Logotipo do Bytebank"
+                        width={26}
+                        height={26}
+                        className={styles.logo_Icone}
+                    />
+                </div>
+                <button className={styles.hamburger} onClick={toggleMenu}>
+                    <FiMenu size={32} />
+                </button>
+                <nav className={`${styles.links} ${menuOpen ? styles.open : ""}`}>
+                    <a href="#">Sobre</a>
+                    <a href="#">Serviços</a>
+                </nav>
+            </div>
+            <ModalButtons variant={"header"} />
+            <ModalApplication />
+        </header>
+    );
+};
+
+export default Header;
