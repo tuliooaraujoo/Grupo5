5b63e09f6eb79f0541920616fba5028c321194c1
Rodrigo Harder <rodrigo.harder@alura.com.br>
2025-01-12 02:27:28 -0300

    feat: criação do componente de extrato

--- projeto-sala2/src/app/dashboard/page.tsx
+++ projeto-sala2/src/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 
+import FormTransaction from "@/components/Forms/FormTransaction"
 import Investments from "@/components/Investments"
 
 export default function Home() {
   return (
     <div>
+<FormTransaction/>
 <Investments/>
     </div>
   )
--- projeto-sala2/src/components/Inputs/SelectorField/index.tsx
+++ projeto-sala2/src/components/Inputs/SelectorField/index.tsx
@@ -1,15 +1,40 @@
-const SelectorField = () => {
-    return (
-        <select
-            name="transacao"
-            id="transacao"
-            className="border border-blue text-base rounded-lg p-3.5 focus-blue w-4/5 max-sm:w-full"
-        >
-            <option value="" disabled selected>Selecione uma transação</option>
-            <option value="Depósito">Depósito</option>
-            <option value="Transferência">Transferência</option>
-        </select>
-    )
-}
+// const SelectorField = () => {
+//     return (
+//         <select
+//             name="transacao"
+//             id="transacao"
+//             className="border border-blue text-base rounded-lg p-3.5 focus-blue w-4/5 max-sm:w-full"
+//         >
+//             <option value="" disabled selected>Selecione uma transação</option>
+//             <option value="Depósito">Depósito</option>
+//             <option value="Transferência">Transferência</option>
+//         </select>
+//     )
+// }
 
-export default SelectorField
+// export default SelectorField
+
+interface SelectorFieldProps {
+    onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+    value: "depósito" | "transferência";
+  }
+  
+  const SelectorField = ({ onChange, value }: SelectorFieldProps) => {
+    return (
+      <select
+        name="transacao"
+        id="transacao"
+        className="border border-blue text-base rounded-lg p-3.5 focus-blue w-4/5 max-sm:w-full"
+        onChange={onChange} // O evento onChange agora é aceito
+        value={value} // O valor da seleção é controlado pelo estado `transactionType`
+      >
+        <option value="" disabled>
+          Selecione uma transação
+        </option>
+        <option value="depósito">Depósito</option>
+        <option value="transferência">Transferência</option>
+      </select>
+    );
+  };
+  
+  export default SelectorField;
