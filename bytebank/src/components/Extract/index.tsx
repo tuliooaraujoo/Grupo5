39a3a742d3741cd6a98a2f21a27cb9c322f8b1c5
WelitonSalgado <weliton.salgado@gmail.com>
2025-03-02 17:55:20 -0300

    Paginação com carrossel para o extrato
    Primeira Versão

--- bytebank/src/components/Extract/index.tsx
+++ bytebank/src/components/Extract/index.tsx
@@ -5,6 +5,20 @@ import { Transaction } from "@/interfaces/transaction";
 import { TransactionFilter } from "./Filter";
 import TransactionPopup from "./TransactionPopup";
 
+
+import React, { useRef } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './styles.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 interface ExtractProps {
   transactions: Transaction[];
   onEdit: (transaction: Transaction) => void;
@@ -69,6 +83,13 @@ const Extract = ({ transactions, onEdit, onDelete }: ExtractProps) => {
     return matchesDate && matchesType && matchesMinValue && matchesMaxValue;
   });
 
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index: any, className: any) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  };
+
   return (
     <div className="bg-lightgray p-6 rounded-lg flex flex-col gap-4">
       <h3 className="text-2xl font-bold">Extrato</h3>
@@ -79,7 +100,42 @@ const Extract = ({ transactions, onEdit, onDelete }: ExtractProps) => {
         {filteredTransactions.length === 0 ? (
           <p>Não há transações correspondentes.</p>
         ) : (
-          <ul>
+          <Swiper
+              pagination={pagination}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              {filteredTransactions.map((transaction) => (
+                    <SwiperSlide key={transaction.id}>
+                      <div className="h-[100px] grid grid-cols-3 grid-rows-3 gap-2 justify-center items-center">
+                        <div className="text-green text-sm font-semibold col-start-1 col-end-2 row-start-1 row-end-2">
+                          {transaction.month}
+                        </div>
+                        <div className="col-start-1 col-end-2 row-start-2 row-end-3">
+                          {transaction.type === "depósito" ? "Depósito" : "Transferência"}
+                        </div>
+                        <div className="col-start-1 col-end-2 row-start-3 row-end-4 text-green font-semibold">
+                          R$ {transaction.value}
+                        </div>
+                        <div className="col-start-3 col-end-4 row-start-2 row-end-3 text-sm text-placeholder">
+                          {transaction.date}
+                        </div>
+                        <div className="col-start-3 col-end-4 row-start-3 row-end-4 flex justify-center items-center gap-4 text-green">
+
+                          {transaction.receiptUrl && (
+                            <a href={transaction.receiptUrl} target="_blank" rel="noopener noreferrer">
+                              <MdFilePresent size={20} />
+                            </a>
+                          )}
+
+                          <Button text={<MdEdit size={20} />} onClick={() => onEdit(transaction)} />
+                          <Button text={<MdDelete size={20} />} onClick={() => transaction.id && onDelete(transaction.id)} />
+                        </div>
+                      </div>
+                      </SwiperSlide>
+                  ))}
+            </Swiper>
+          /*<ul>
             {filteredTransactions.map((transaction) => (
               <li key={transaction.id} className="border-b-2 border-green">
                 <div className="h-[100px] grid grid-cols-3 grid-rows-3 gap-2 justify-center items-center">
@@ -109,7 +165,7 @@ const Extract = ({ transactions, onEdit, onDelete }: ExtractProps) => {
                 </div>
               </li>
             ))}
-          </ul>
+          </ul>*/
         )}
       </div>
 
