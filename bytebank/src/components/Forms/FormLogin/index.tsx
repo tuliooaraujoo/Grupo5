91f669a5476bcf0aeb9db54dc21dec06e80c2448
Rodrigo Harder <rodrigo.harder@alura.com.br>
2025-03-12 10:40:34 -0300

    implementando apenas 1 comando para iniciar o back e o front

--- bytebank/src/components/Forms/FormLogin/index.tsx
+++ bytebank/src/components/Forms/FormLogin/index.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 import login from "../../../../public/images/illustrations/login.svg";
 import TextField from "@/components/Inputs/TextField";
 import Button from "@/components/Button";
-import useLogin from "@/hooks/UseLogin";
+import useLogin from "@/hooks/useLogin";
+
 
 const FormLogin = () => {
     const { email, password, error, setEmail, setPassword, handleLogin } = useLogin();
